a169df7b4ca3058464d69c75de5e6ad1728c45b9
Naoise Collins <47043016+naoisecollins@users.noreply.github.com>
2023-03-10 18:54:24 +0000

    added js and css

---
+++ main.js
@@ -0,0 +1,21 @@
+// Get the player element and initialize its position
+const player = document.getElementById("player");
+let playerX = 0;
+
+// Define the keydown event listener to handle player movement
+document.addEventListener("keydown", event => {
+  switch (event.key) {
+    case "ArrowLeft":
+      playerX -= 10;
+      break;
+    case "ArrowRight":
+      playerX += 10;
+      break;
+    default:
+      return;
+  }
+  event.preventDefault();
+
+  // Update the player's position
+  player.style.left = `${playerX}px`;
+});
